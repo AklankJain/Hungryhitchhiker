e19691664eefaa0511af54d44d6a37fb15386d68
Aklank Jain <ajain9@yodlee.com>
2018-04-09 23:41:09 +0530

    carousel changes

--- src/jsx/slider.js
+++ src/jsx/slider.js
@@ -10,15 +10,15 @@ class Slider extends React.Component {
     return( 
 <div style={{ background: '#fff', padding: 24 }}>
 <Carousel afterChange={this.onChange}>
-    		<div><img src ="https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/18156636_10212870205168957_6670301087148277348_o.jpg"  height = "450px" width = "100%"/></div>
-    		<div><img src ="https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/IMG_20171112_122857.jpg"  height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/IMG_20170713_170403.jpg"  height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/DSC_5255.JPG" height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/DSC_0180.JPG" height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/DSC_0134.JPG" height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/23970151_902959716529463_5847621355305959424_n.jpg" height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/23507116_1754083227937188_8963694221034782720_n.jpg" height = "450px" width = "100%"/></div>
-    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/18299919_2056935194542227_8228411174467665920_n.jpg" height = "450px" width = "100%"/></div>
+    		<div><img src ="https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/18156636_10212870205168957_6670301087148277348_o.jpg"   width = "100%"/></div>
+    		<div style={{ }}><img src ="https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/IMG_20171112_122857.jpg" height = "100%"  /></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/IMG_20170713_170403.jpg"  height = "100%"/></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/DSC_5255.JPG"  width = "100%"/></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/DSC_0180.JPG"  width = "100%"/></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/DSC_0134.JPG"  width = "100%"/></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/23970151_902959716529463_5847621355305959424_n.jpg"  width = "100%"/></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/23507116_1754083227937188_8963694221034782720_n.jpg"  height = "100%"/></div>
+    		<div><img src = "https://s3.ap-south-1.amazonaws.com/hungry-media/home-page-photos/18299919_2056935194542227_8228411174467665920_n.jpg"  height = "100%"/></div>
   		</Carousel>
 
 </div>
