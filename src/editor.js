5218c4103235cdb5b5c0bf825c3151c453673a39
Aklank Jain <ajain9@yodlee.com>
2019-03-12 23:10:14 +0530

    added the edit option in the editor options

--- src/editor.js
+++ src/editor.js
@@ -3,7 +3,8 @@ import React , {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import RichTextEditor from 'react-rte';
-import { Editor } from '@tinymce/tinymce-react';
+import { Editor , tinymce } from '@tinymce/tinymce-react';
+// import TinyMCE from 'react-tinymce';
 import './app.css'
 import axios from 'axios'
 import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete, Radio } from 'antd';
@@ -14,13 +15,27 @@ const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 const { TextArea } = Input;
 
+var flag = 0
 var ti = ""
+var te = ""
+var content_editor = ""
+var edit_title = ""
+var edit_tags = ""
+var edit_key = ""
 class RegistrationForm extends React.Component {
   static propTypes = {
     onChange: PropTypes.func
   };
   state = {
-    value: RichTextEditor.createEmptyValue()
+    value: RichTextEditor.createEmptyValue(),
+    activeEditor : "empty value",
+    content : ""
+  }
+
+
+  constructor(props){
+    super(props);
+    this.onChange = this.onChange.bind(this);
   }
 
 
@@ -38,10 +53,95 @@ class RegistrationForm extends React.Component {
     }
   };
 
+  getPostToBeEdited = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll(['title_edit' , 'tag_edit'],(err, values) => {
+      var key_check = values.title_edit + values.tag_edit;
+      edit_key = key_check
+      edit_tags = values.tag_edit
+      edit_title = values.title_edit
+      console.log(key_check);
+      if (!err) {
+        console.log('Received values of form: ', values);
+        const ax_url = 'https://server-try.herokuapp.com/api/update/';
+        axios.get(ax_url)
+        .then(res => {
+          this.setState({ hungry_rides : res.data[8].hungry_rides.reverse() ,
+            food_walks : res.data[8].food_walks.reverse(),
+            about : res.data[8].about
+          });
+          console.log("this is tag_edit" , values.tag_edit)
+          if(values.tag_edit == 'food_walks'){
+          var food_walks = this.state.food_walks || []
+          food_walks.forEach(function(item, index){
+            console.log(item.key)
+            if (key_check == item.key){
+              console.log("found a match, which is " ,item.key)
+              // console.log(this.state.activeEditor);
+              window.Editor.setContent("this is the content to be set");
+            }
+          })
+        }
+        else if (values.tag_edit == 'hungry_rides') {
+          var hungry_rides = this.state.hungry_rides || []
+          hungry_rides.forEach(function(item, index){
+            // console.log(item.key)
+            if (key_check == item.key){
+              console.log("found a match, which is " ,item.key)
+              // console.log(this.state.activeEditor);
+              // _.tinymce.get('edit_editor').setContent("this is the content to be set");
+              console.log("is the data set, that is the question")
+              content_editor = item.content;
+              // this.state.content = item.content;
+            }
+          })
+        }
+          // console.log(this.state.food_walks);
+    });
+  }
+    else {
+      console.log("there is an error in edit form ");
+    }
+  });
+}
+
   handleEditorChange = (e) => {
     // console.log('Content was updated:', e.target.getContent());
     ti = e.target.getContent()
     console.log(ti)
+  }
+
+  UpdateEditorValues = (e) => {
+    e.preventDefault();
+    axios.put('https://server-try.herokuapp.com/api/delete/', {titled: edit_title, tagged: edit_tags})
+    .then(response => {
+      console.log(response);
+      axios.put('https://server-try.herokuapp.com/api/update', {title: edit_title,content: te , tags: edit_tags,key: edit_key})
+      .then(response => {
+        console.log(response);
+        alert("Submitted")
+          })
+        .catch(error => {
+           console.log("there is an error in the field");
+          });
+        })
+      .catch(error => {
+         console.log("there is some error in updating. please check ");
+        });
+
+  }
+
+  checking = (e) => {
+    // console.log('Content was updated:', e.target.getContent());
+    if(flag ==0){
+    e.target.setContent(content_editor);
+    flag = 1
+  }
+  else if (flag == 1) {
+    te = e.target.getContent()
+    console.log(te)
+  }
+    // console.log(ti)
   }
 
   handleDeleteSubmit = (e) => {
@@ -56,7 +156,7 @@ class RegistrationForm extends React.Component {
             })
           .catch(error => {
              console.log(err);
-            });      
+            });
       }
     });
   }
@@ -76,7 +176,7 @@ class RegistrationForm extends React.Component {
             })
           .catch(error => {
              console.log(err);
-            });      
+            });
       }
     });
   }
@@ -114,6 +214,7 @@ class RegistrationForm extends React.Component {
       <div>
       <Editor
         initialValue="<p>This is the initial content of the editor</p>"
+        id="edit_editor"
         init={{
           plugins: ['advlist autolink lists link image charmap print preview hr anchor pagebreak',
     'searchreplace wordcount visualblocks visualchars code fullscreen',
@@ -193,6 +294,68 @@ class RegistrationForm extends React.Component {
           <Button type="primary" htmlType="submit">Submit</Button>
         </FormItem>
         </Form>
+        <Form onSubmit={this.getPostToBeEdited} >
+        <h1>To edit a post, give it here.</h1>
+        <FormItem
+          {...formItemLayout}
+          label="Title" >
+          {getFieldDecorator('title_edit', {
+            rules: [{
+              required: true, message: 'Please input the title',
+            }],
+          })(
+            <Input />
+          )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="Tagged"
+          >
+            {getFieldDecorator('tag_edit' , {rules: [{
+                required: true, message: 'Please input the content!',
+              }],
+            })(
+              <RadioGroup>
+                <RadioButton value="hungry_rides">Hungry Rides</RadioButton>
+                <RadioButton value="food_walks">Hiker's Diary</RadioButton>
+              </RadioGroup>
+            )}
+          </FormItem>
+          <FormItem {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">Submit</Button>
+          </FormItem>
+          </Form>
+          <Form onSubmit={this.UpdateEditorValues}>
+          <FormItem>
+          <Editor
+            // initialValue = ""
+            initialValue="<p>Delete it to get the data inside the editor</p>"
+            // id="edit_editor"
+            init={{
+              // setup: editor => {
+              //   this.setState({activeEditor: editor}) },
+              id:"edit_editor",
+              plugins: ['advlist autolink lists link image charmap print preview hr anchor pagebreak',
+        'searchreplace wordcount visualblocks visualchars code fullscreen',
+        'insertdatetime media nonbreaking save table contextmenu directionality',
+        'emoticons template paste textcolor colorpicker textpattern imagetools'
+        ],
+
+              toolbar: 'undo redo | bold italic underline | alignleft aligncenter alignright | code',
+              image_advtab: true,
+              content_css: ['//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+        '//www.tinymce.com/css/codepen.min.css'
+       ]
+            }}
+            onChange={this.checking}
+
+            // onClick={this.checking}
+          />
+          </FormItem>
+          <FormItem {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">Update</Button>
+          </FormItem>
+        </Form>
       </div>
     );
   }
@@ -203,7 +366,7 @@ const WrappedRegistrationForm = Form.create()(RegistrationForm);
 
 class MyEditor extends React.Component {
   render(){
-    return( 
+    return(
       <div id = "editor">
       <h1> content Form </h1>
      <WrappedRegistrationForm />
@@ -211,7 +374,6 @@ class MyEditor extends React.Component {
     )
 }
 }
-
 
 
 export default MyEditor
